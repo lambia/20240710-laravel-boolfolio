5dc33e971d40640d4b2ec5bcd420ef27186b3ccb
Luca <luca@lambia.it>
2024-07-10 13:06:04 +0200

    add working model, migration and seeder

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i=0; $i < 10; $i++) { 
+            $newProject = new Project();
+            $newProject->name = $faker->sentence(2);
+            $newProject->description = $faker->sentence(22);
+            $newProject->img = "https://picsum.photos/id/" . rand(1, 500) . "/600/600";
+            $newProject->save();
+        }
+    }
+}
