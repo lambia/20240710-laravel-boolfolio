48f5427ac0877f45c4857dfc17c382cfb4e80aa6
Luca <luca@lambia.it>
2024-07-11 14:52:55 +0200

    improve category show layout

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -5,9 +5,12 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
 
-                <h1 class="pb-4">{{ $categoria->name }}</h1>
-                <i class="{{ $categoria->icon }}"></i>
-                <br />
+                <h1 class="pb-4">
+                    <i class="{{ $categoria->icon }}"></i> {{ $categoria->name }}
+                </h1>
+                
+                
+                
                 <a href="{{ route("admin.categories.index") }}" class="btn btn-primary">Torna alla lista categorie</a>
 
             </div>
