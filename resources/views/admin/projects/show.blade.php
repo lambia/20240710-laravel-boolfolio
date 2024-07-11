5bd2448b20fb4d9b561de331f727f14e00af6d23
Luca <luca@lambia.it>
2024-07-11 14:50:22 +0200

    BONUS: recupero relazione MANUALMENTE

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -8,7 +8,13 @@
                 <h1 class="pb-4">{{ $progetto->name }}</h1>
                 <img src="{{ $progetto->img }}" class="w-100" alt="{{ $progetto->name }}">
                 <p class="card-text my-4">{{ $progetto->description }}</p>
-                <p class="card-text my-4">Categoria: {{ $progetto->category_id }}</p>
+
+                {{-- Stampo semplicemente l'intero scritto nella colonna --}}
+                <p class="card-text my-4">ID Categoria: {{ $progetto->category_id }}</p>
+
+                {{-- Stampo la Category collegata, recuperata a mano dal Controller --}}
+                <p class="card-text my-4">Categoria: {{ $progetto->categoriaPresaManualmente->name }}</p>
+                
                 <a href="{{ route("admin.projects.index") }}" class="btn btn-primary">Torna alla lista progetti</a>
 
             </div>
